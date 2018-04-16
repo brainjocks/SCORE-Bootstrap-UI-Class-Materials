61d520a129860f115cb76f664cae80f00cc46870
Ben Cato <bcato3000@gmail.com>
2018-04-16 09:11:02 -0400

    Update class files
    Ready for SCORE UI 3.0 class

---
+++ RenameMeArea/js/require.config.js
@@ -0,0 +1,16 @@
+﻿;(function(global) {
+
+    global.require = global.require || {};
+
+    // paths
+    global.require.paths = global.require.paths || {};
+    global.require.paths.RenameMeArea = "/Areas/RenameMeArea/js";
+
+    // shim
+    global.require.shim = global.require.shim || {};
+
+    // map
+    global.require.map = global.require.map || {};
+    global.require.map["*"] = global.require.map["*"] || {};
+
+})(this /* window */);
